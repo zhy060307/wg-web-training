f6792f2bf92206772273ac67c0ebabfc61bc1654
zhaohaiyang <zhaohaiyang@wisdomgarden.com>
2019-07-28 23:26:05 +0800

    add card view ui.

---
+++ src/components/CardView.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="content">
+        <div class="head">
+            <div class="title">{{title}}</div>
+            <div class="present" :class="up?redColor:greenColor">{{percent}}</div>
+        </div>
+
+        <div class="total">{{total}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'card-view',
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            percent: {
+                type: String,
+                required: true
+            },
+            total: {
+                type: String,
+                required: true
+            },
+            up: {
+                type: Boolean,
+                required: false,
+                default: false
+            }
+
+        },
+
+        data() {
+            return {
+                redColor: 'red',
+                greenColor: 'green'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .red {
+        color: red;
+    }
+
+    .green {
+        color: green;
+    }
+
+    .content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+
+        color: #737373;
+        padding: 2.1rem 2.4rem;
+        margin: 0;
+        min-width: 10rem;
+        line-height: 1;
+
+        .head {
+            display: flex;
+            flex-direction: row;
+
+            .title {
+                margin-right: 1.2rem;
+
+            }
+        }
+
+        .total {
+            margin-top: 1.2rem;
+            font-size: 2.4rem;
+            color: #333;
+        }
+    }
+
+</style>
--- src/components/FirstCourse.vue
+++ src/components/FirstCourse.vue
@@ -1,9 +1,37 @@
 <template>
     <div>
-        <h2>first course</h2>
+        <div class="card-head-title">访问分析</div>
+        <div class="card-tabs">
+            <card-view title="访问总量" percent="-26%" total="22,621"/>
+            <card-view title="访问课数" percent="-11%" total="947"/>
+            <card-view title="访问总时长" percent="26%" up="true" total="1,781:35:53"/>
+        </div>
+
+
     </div>
 </template>
 
 <script>
-    export default {}
+    import CardView from "./CardView.vue";
+
+    export default {
+        components: {CardView},
+        data() {
+            return {}
+        }
+
+    }
 </script>
+<style lang="scss">
+
+    .card-head-title {
+        padding: 1.9rem 2.4rem;
+        border-bottom: 1px solid #e8e8e8;
+        background-color: #fff;
+    }
+
+    .card-tabs {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
